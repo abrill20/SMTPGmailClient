e19e9e9dfabd99aaa5820efe4c1596549f2883b6
Aaron Brill <abrill@skidmore.edu>
2019-04-05 13:18:50 -0400

    Add main method

--- SMTPClient.py
+++ SMTPClient.py
@@ -1,113 +1,117 @@
 import socket
 import base64, ssl
 
-# Choose a mail server (e.g. Google mail server) and call it mail server
-mailserver = "smtp.gmail.com"
-# Create socket called client Socket and establish a TCP connection with mail server
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket = ssl.wrap_socket(clientSocket)
-print('====================================')
-print('Establishing connection...')
-clientSocket.connect((mailserver,465))
-recv = clientSocket.recv(1024).decode()
-print("Server: ", recv)
-if recv[:3] != '220':
-  print('220 reply not received from server.')
-# Send HELO command and print server response.
-heloCommand = 'HELO smtp.gmail.com \r\n'
-print('====================================')
-print('Client: ', heloCommand)
-clientSocket.send(heloCommand.encode())
-recv = clientSocket.recv(1024).decode()
-print("Server: ", recv)
-if recv[:3] != '250':
-  print('250 reply not received from server.')
+def Main():
+  # Choose a mail server (e.g. Google mail server) and call it mail server
+  mailserver = "smtp.gmail.com"
+  # Create socket called client Socket and establish a TCP connection with mail server
+  clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  clientSocket = ssl.wrap_socket(clientSocket)
+  print('====================================')
+  print('Establishing connection...')
+  clientSocket.connect((mailserver,465))
+  recv = clientSocket.recv(1024).decode()
+  print("Server: ", recv)
+  if recv[:3] != '220':
+    print('220 reply not received from server.')
+  # Send HELO command and print server response.
+  heloCommand = 'HELO smtp.gmail.com \r\n'
+  print('====================================')
+  print('Client: ', heloCommand)
+  clientSocket.send(heloCommand.encode())
+  recv = clientSocket.recv(1024).decode()
+  print("Server: ", recv)
+  if recv[:3] != '250':
+    print('250 reply not received from server.')
 
 
-################################
-# Authentication
-################################
+  ################################
+  # Authentication
+  ################################
 
-username = input("Enter gmail username: ")
-password = input("Enter gmail password: ")
+  username = input("Enter gmail username: ")
+  password = input("Enter gmail password: ")
 
-authCommand = 'AUTH LOGIN\r\n'
-print('====================================')
-print('Client: ', authCommand)
-clientSocket.send(authCommand.encode())
-recv = clientSocket.recv(1024).decode()
-print('Server: ', recv)
-username64 = base64.b64encode(username.encode())
-password64 = base64.b64encode(password.encode())
-print('Client: username (b64 encoded)\r\n')
-clientSocket.send(username64)
-clientSocket.send('\r\n'.encode())
-recv = clientSocket.recv(1024).decode()
-print('Server: ', recv)
-print('Client: password (b64 encoded)\r\n')
-clientSocket.send(password64)
-clientSocket.send('\r\n'.encode())
-recv = clientSocket.recv(1024).decode()
-print('Server: ', recv)
+  authCommand = 'AUTH LOGIN\r\n'
+  print('====================================')
+  print('Client: ', authCommand)
+  clientSocket.send(authCommand.encode())
+  recv = clientSocket.recv(1024).decode()
+  print('Server: ', recv)
+  username64 = base64.b64encode(username.encode())
+  password64 = base64.b64encode(password.encode())
+  print('Client: username (b64 encoded)\r\n')
+  clientSocket.send(username64)
+  clientSocket.send('\r\n'.encode())
+  recv = clientSocket.recv(1024).decode()
+  print('Server: ', recv)
+  print('Client: password (b64 encoded)\r\n')
+  clientSocket.send(password64)
+  clientSocket.send('\r\n'.encode())
+  recv = clientSocket.recv(1024).decode()
+  print('Server: ', recv)
 
-################################
-# Send MAIL FROM command and print server response.
-################################
-mailFrom = username
+  ################################
+  # Send MAIL FROM command and print server response.
+  ################################
+  mailFrom = username
 
-mailFromCommand = 'MAIL FROM:<' + mailFrom + '>\r\n'
-print('====================================')
-print('Client: ', mailFromCommand)
-clientSocket.send(mailFromCommand.encode())
-recv = clientSocket.recv(1024).decode()
-print('Server: ', recv)
+  mailFromCommand = 'MAIL FROM:<' + mailFrom + '>\r\n'
+  print('====================================')
+  print('Client: ', mailFromCommand)
+  clientSocket.send(mailFromCommand.encode())
+  recv = clientSocket.recv(1024).decode()
+  print('Server: ', recv)
 
-################################
-# Send RCPT TO command and print server response.
-################################
-rcptTo = input("Who would you like to send this email to? ")
+  ################################
+  # Send RCPT TO command and print server response.
+  ################################
+  rcptTo = input("Who would you like to send this email to? ")
 
-rcptToCommand = 'RCPT TO:<' + rcptTo + '>\r\n'
-print('====================================')
-print('Client: ', rcptToCommand)
-clientSocket.send(rcptToCommand.encode())
-recv = clientSocket.recv(1024).decode()
-print('Server: ', recv)
+  rcptToCommand = 'RCPT TO:<' + rcptTo + '>\r\n'
+  print('====================================')
+  print('Client: ', rcptToCommand)
+  clientSocket.send(rcptToCommand.encode())
+  recv = clientSocket.recv(1024).decode()
+  print('Server: ', recv)
 
-################################
-# Send DATA command and print server response.
-################################
+  ################################
+  # Send DATA command and print server response.
+  ################################
 
-dataCommand = 'DATA\r\n'
-print('====================================')
-print('Client: ', dataCommand)
-clientSocket.send(dataCommand.encode())
-recv = clientSocket.recv(1024).decode()
-print('Server: ', recv)
+  dataCommand = 'DATA\r\n'
+  print('====================================')
+  print('Client: ', dataCommand)
+  clientSocket.send(dataCommand.encode())
+  recv = clientSocket.recv(1024).decode()
+  print('Server: ', recv)
 
-################################
-# Send message data.
-################################
+  ################################
+  # Send message data.
+  ################################
 
-message = input("Enter a message: ")
+  message = input("Enter a message: ")
 
-message = message + '\r\n\r\n.\r\n'
+  message = message + '\r\n\r\n.\r\n'
 
-print('====================================')
-print('Client: ', message)
-clientSocket.send(message.encode())
-recv = clientSocket.recv(1024).decode()
-print('Server: ', recv)
+  print('====================================')
+  print('Client: ', message)
+  clientSocket.send(message.encode())
+  recv = clientSocket.recv(1024).decode()
+  print('Server: ', recv)
 
-################################
-# Send QUIT command and get server response.
-################################
+  ################################
+  # Send QUIT command and get server response.
+  ################################
 
-quitCommand = 'QUIT\r\n'
-print('====================================')
-print('Client: ', quitCommand)
-clientSocket.send(quitCommand.encode())
-recv = clientSocket.recv(1024).decode()
-print('Server: ', recv)
+  quitCommand = 'QUIT\r\n'
+  print('====================================')
+  print('Client: ', quitCommand)
+  clientSocket.send(quitCommand.encode())
+  recv = clientSocket.recv(1024).decode()
+  print('Server: ', recv)
 
-clientSocket.close()
+  clientSocket.close()
+
+if __name__ == '__main__':
+  Main()
